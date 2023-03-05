55f26503421b5e970078147d58931631ac6380e2
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2023-03-05 22:32:50 +0100

    mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
+import CharacterLengh from "./modules/HomeModule/components/CharacterLengh";
+import CheckboxContainer from "./modules/HomeModule/components/CheckboxContainer";
 
 const App: React.FC = () => (
   <>
-  <h1>Hello world</h1>
+    <div className="main">
+      <h1 className="main__title">Password Generator</h1>
+      <CharacterLengh />
+      <CheckboxContainer />
+    </div>
   </>
 );
 
---
+++ src/modules/HomeModule/components/CheckboxContainer.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const CheckboxContainer = () => {
+  return (
+    <div className="section">
+        <div className="section__row">
+            <input type="checkbox" className="section_checkbox" />
+            <p>Include Uppercase Letter</p>
+        </div>
+        <div className="section__row">
+            <input type="checkbox" className="section_checkbox" />
+            <p>Include Uppercase Letter</p>
+        </div>
+        <div className="section__row">
+            <input type="checkbox" className="section_checkbox" />
+            <p>Include Uppercase Letter</p>
+        </div>
+        <div className="section__row">
+            <input type="checkbox" className="section_checkbox" />
+            <p>Include Uppercase Letter</p>
+        </div>
+    </div>
+  )
+}
+
+export default CheckboxContainer
